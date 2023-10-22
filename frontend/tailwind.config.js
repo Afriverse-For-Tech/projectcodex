6fc5f5a4423b3fadba75a7550c2de2625cda48d7
Daniel Olagunju <todak2000@gmail.com>
2023-10-22 19:41:10 +0100

    initial fe setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: ['flowbite/plugin'],
+}
+
