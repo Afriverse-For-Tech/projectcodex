bf48e0e835e31b3875970c7d749347dc0983d1a3
alexindevs <alexindevs@gmail.com>
2023-11-11 03:28:12 +0100

    Feat: Join Waitlist API endpoint

--- backend/nodejs/tests/hello-world.test.js
+++ backend/nodejs/tests/hello-world.test.js
@@ -1,22 +1,20 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('GET /api/v1', () => {
-  let server; // Declare a variable to hold the server instance
+describe('GET /', () => {
+  let server;
 
   beforeAll(() => {
-    // Start the server before running tests
     server = app.listen(3000);
   });
 
   afterAll((done) => {
-    // Close the server after all tests are done
     server.close(done);
   });
 
-  it('responds with "Hello World"', async () => {
-    const response = await request(app).get('/api/v1');
+  it('responds properly', async () => {
+    const response = await request(app).get('/');
     expect(response.status).toBe(200);
-    expect(response.text).toBe('Hello World');
+    expect(response.text).toBe('Welcome to the API! Please visit /api-docs to see our documentation.');
   });
 });
